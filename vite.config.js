97df45e32509e78ea356efd30378fcc64602d7f4
patrick7177 <impatricktaylor@gmail.com>
2024-12-11 20:25:05 -0500

    Switch to styled-components for MUI styling

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-    babel: {
-      plugins: ['@emotion/babel-plugin']
-    }
-  })],
-  build: {
-    rollupOptions: {
-      external: ['@emotion/react/jsx-runtime']
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui/styled-engine-sc'
     }
   }
 })
