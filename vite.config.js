ee8f468f3b3eac582bdc3d3aeae52ab69097c47b
patrick7177 <impatricktaylor@gmail.com>
2024-12-11 20:22:16 -0500

    Fix Vite config for MUI and Emotion

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxImportSource: '@emotion/react',
+    babel: {
+      plugins: ['@emotion/babel-plugin']
+    }
+  })],
   build: {
     rollupOptions: {
-      external: ['@emotion/react/jsx-runtime'],
-    },
-  },
-  resolve: {
-    alias: {
-      '@mui/styled-engine': '@mui/styled-engine-sc',
-    },
-  },
+      external: ['@emotion/react/jsx-runtime']
+    }
+  }
 })
