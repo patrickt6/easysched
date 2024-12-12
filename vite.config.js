a13ca748e01b8b2347ba31860e0353a7f0de9022
patrick7177 <impatricktaylor@gmail.com>
2024-12-11 20:20:07 -0500

    Add MUI dependencies and update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@emotion/react/jsx-runtime'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui/styled-engine-sc',
+    },
+  },
 })
