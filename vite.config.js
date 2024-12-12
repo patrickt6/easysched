e1aea832e099bf5d3777887bdd8bf6deefc83a97
patrick7177 <impatricktaylor@gmail.com>
2024-12-11 20:48:48 -0500

    Fix date-fns build issue by adding commonjs plugin

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from '@rollup/plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    commonjs({
+      include: /node_modules/,
+    }),
+    react()
+  ],
   resolve: {
     alias: {
       '@mui/styled-engine': '@mui/styled-engine-sc'
     }
+  },
+  optimizeDeps: {
+    include: ['date-fns']
   }
 })
